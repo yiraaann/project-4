e163d33fc50b34604be16c006a4a2d778a4d518d
yiraaann <yiran.research@gmail.com>
2025-03-03 16:07:13 -0600

    update code

--- Zhang - Project 4/api_keys.py
+++ Zhang - Project 4/api_keys.py
@@ -2,3 +2,7 @@
 client_id = 'WF9hikTjHUy4P-bu_b9cebYWxUeuOmbeL_WvhxTCr0RIDQaTqDbUWeXjspjnUPLC'
 client_secret = 'S8fh1NAqO0JRogGToqG5AN4BgrbSodwJAxU0L4_86Eok0x1gv_ws4tM-c2i7H9iTTYyDj2nZ-oM_Dl9lMHWVqw'
 access_token = 'mln_UHIoMnrd1gXTZiIq-Qh2xhLTz1jlz0qLT6B9eOdujchncqbKD09HM1R3v2Vo'
+
+# Spotify access tokens
+spotify_client_id = 'd2d08a4ffe8c4890ab8d2db864f6da74'
+spotify_client_secret = 'b1f6441bea7343b8a8fd359a7c5b903c'
